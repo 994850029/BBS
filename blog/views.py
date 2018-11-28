b9c190f1329cb284dac8e5e609133ba245e59174
zhoujianhao <zhoujianhao1996@163.com>
2018-11-28 15:15:20 +0800

    验证码修缮

--- blog/views.py
+++ blog/views.py
@@ -15,27 +15,31 @@ def login(request):
     if request.method == 'POST':
         # blog = models.Blog.objects.create(site_name='test',theme='test')
         # models.UserInfo.objects.create_user(username='zjh',password='123',phone='12345',blog=blog)
-        msg = json.loads(request.body.decode('utf-8'))
+        # msg = json.loads(request.body.decode('utf-8'))
+        msg = request.POST
         user = msg.get('name')
         pwd = msg.get('pwd')
         yzm_session = request.session.get('yzm')
         yzm = msg.get('yzm')
+        print(yzm_session)
         pd = auth.authenticate(username=user,password=pwd)
-        if pd and yzm == yzm_session:
-            return HttpResponse(json.dumps('登录成功!'))
+        if pd and yzm.upper() == yzm_session.upper():
+            # return HttpResponse(json.dumps('登录成功!'))
+            return HttpResponse('登录成功!')
         else:
-            return HttpResponse(json.dumps('用户名或者密码错误!'))
+            # return HttpResponse(json.dumps('用户名或者密码错误!'))
+            return HttpResponse('用户名或者密码错误!')
 
 
 def get_img(request):
     img = Image.new('RGB',(300,30),color=common.get_random_color())
-    font = ImageFont.truetype(font='static/font/txt.TTF',size=18)
+    font = ImageFont.truetype(font='static/font/txt.TTF',size=24)
     img_draw = ImageDraw.Draw(img)
     font_content = common.get_random_yzm(5)
-
     request.session['yzm'] = font_content
     img_draw.text((100,0),font_content,common.get_random_color(),font=font)
-    f= BytesIO()
+    common.add_dianxiang(300,30,img_draw)
+    f = BytesIO()
     img.save(f,'png')
     data = f.getvalue()
 
--- static/common/common.py
+++ static/common/common.py
@@ -14,6 +14,24 @@ def get_random_yzm(i):
         S = chr(random.randint(65,90))
         s = chr(random.randint(97,122))
         c= random.choice([num,S,s])
-        res +=c
+        res += c
 
     return res
+
+def add_dianxiang(width,height,img_draw):
+    for i in range(5):
+        x1 = random.randint(0, width)
+        x2 = random.randint(0, width)
+        y1 = random.randint(0, height)
+        y2 = random.randint(0, height)
+        # 在图片上画线
+        img_draw.line((x1, y1, x2, y2), fill=get_random_color())
+
+    for i in range(10):
+        # 画点
+        img_draw.point([random.randint(0, width), random.randint(0, height)], fill=get_random_color())
+        x = random.randint(0, width)
+        y = random.randint(0, height)
+        # 画弧形
+        img_draw.arc((x, y, x + 4, y + 4), 0, 90, fill=get_random_color())
+
